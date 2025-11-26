e333c1cbdd32ff4d6879ae5e63fb40fea5d9980b
NAMRUTHA SRINIDHI T <anamruthasrinidhi@gmail.com>
2025-11-26 16:35:57 +0530

    Update MenuItem.java

--- coffeeMachine/src/coffee/MenuItem.java
+++ coffeeMachine/src/coffee/MenuItem.java
@@ -1,38 +1,19 @@
 package coffee;
 
+import java.math.BigDecimal;
+
 public class MenuItem {
+    private final String name;
+    private final BigDecimal cost;
+    private final Ingredients ingredients;
 
-    private String name;
-    private double cost;
-    private int water;
-    private int milk;
-    private int coffee;
-
-    public MenuItem(String name, int water, int milk, int coffee, double cost) {
+    public MenuItem(String name, BigDecimal cost, Ingredients ingredients) {
         this.name = name;
-        this.water = water;
-        this.milk = milk;
-        this.coffee = coffee;
         this.cost = cost;
+        this.ingredients = ingredients;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public double getCost() {
-        return cost;
-    }
-
-    public int getWater() {
-        return water;
-    }
-
-    public int getMilk() {
-        return milk;
-    }
-
-    public int getCoffee() {
-        return coffee;
-    }
+    public String getName() { return name; }
+    public BigDecimal getCost() { return cost; }
+    public Ingredients getIngredients() { return ingredients; }
 }
