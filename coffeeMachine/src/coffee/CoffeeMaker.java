066ef1b2f98ff7a85580642faab16c606b1b3b28
NAMRUTHA SRINIDHI T <anamruthasrinidhi@gmail.com>
2025-11-26 16:32:57 +0530

    Update CoffeeMaker.java

--- coffeeMachine/src/coffee/CoffeeMaker.java
+++ coffeeMachine/src/coffee/CoffeeMaker.java
@@ -1,38 +1,20 @@
-package coffee;
-
 public class CoffeeMaker {
 
-    private int water = 300;
-    private int milk = 250;
-    private int coffee = 100;
-
-    public void report() {
-        System.out.println("Water: " + water + "ml");
-        System.out.println("Milk: " + milk + "ml");
-        System.out.println("Coffee: " + coffee + "g");
-    }
+    private Ingredients resources = new Ingredients(1000, 800, 500);
 
     public boolean isResourceSufficient(MenuItem drink) {
-        if (drink.getWater() > water) {
-            System.out.println("Sorry there is not enough water.");
-            return false;
-        }
-        if (drink.getMilk() > milk) {
-            System.out.println("Sorry there is not enough milk.");
-            return false;
-        }
-        if (drink.getCoffee() > coffee) {
-            System.out.println("Sorry there is not enough coffee.");
-            return false;
-        }
-        return true;
+        return resources.isEnough(drink.getIngredients());
     }
 
-    public void makeCoffee(MenuItem order) {
-        water -= order.getWater();
-        milk -= order.getMilk();
-        coffee -= order.getCoffee();
+    public void makeCoffee(MenuItem drink) {
+        resources.deduct(drink.getIngredients());
+    }
 
-        System.out.println("Here is your " + order.getName() + " ☕️. Enjoy!");
+    public Ingredients getResources() {
+        return resources;
+    }
+
+    public void refill(Ingredients additional) {
+        resources.add(additional);
     }
 }
